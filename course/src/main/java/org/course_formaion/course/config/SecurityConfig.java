085c84f8c95bf211c314311f2b9fd36c547458b3
Mohamed Abdellahi Soueidatt <mohamedabdellahi.soueidatt@etudiant.univ-brest.fr>
2026-03-13 09:20:09 +0100

    fix course security

--- course/src/main/java/org/course_formaion/course/config/SecurityConfig.java
+++ course/src/main/java/org/course_formaion/course/config/SecurityConfig.java
@@ -2,12 +2,6 @@ package org.course_formaion.course.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
